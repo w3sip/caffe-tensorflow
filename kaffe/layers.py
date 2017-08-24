175fcecece2617bd46c017a1ed5d0c10a2a6ddc8
Eylon Stroh <estroh@adobe.com>
2017-08-23 22:53:25 -0700

    Bug fix for pad_w
    This bug doesn't manifest when specifying only pad rather than pad_h
    and pad_w.

--- kaffe/layers.py
+++ kaffe/layers.py
@@ -143,7 +143,7 @@ class LayerAdapter(object):
         s_h = self.get_kernel_value(params.stride_h, params.stride, 0, default=1)
         s_w = self.get_kernel_value(params.stride_w, params.stride, 1, default=1)
         p_h = self.get_kernel_value(params.pad_h, params.pad, 0, default=0)
-        p_w = self.get_kernel_value(params.pad_h, params.pad, 1, default=0)
+        p_w = self.get_kernel_value(params.pad_w, params.pad, 1, default=0)
         return KernelParameters(k_h, k_w, s_h, s_w, p_h, p_w)
 
 
