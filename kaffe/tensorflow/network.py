6735be3db5c04c4dd49df084c6b1f6faaf47fa93
Eylon Stroh <estroh@adobe.com>
2017-08-24 17:14:17 -0700

    Softmax can handle Tensors of rank > 2

--- kaffe/tensorflow/network.py
+++ kaffe/tensorflow/network.py
@@ -281,8 +281,7 @@ class Network(object):
             # in TensorFlow's NHWC ordering (unlike Caffe's NCHW).
             if input_shape[1] == 1 and input_shape[2] == 1:
                 input = tf.squeeze(input, squeeze_dims=[1, 2])
-            else:
-                raise ValueError('Rank 2 tensor input expected for softmax!')
+            
         return tf.nn.softmax(input, name=name)
 
     @layer
